5faa99faee058a49c5a76e6d78fefe6de18e9135
Felix Guerrero <felixcriv@gmail.com>
2015-02-24 19:35:14 -0500

    support for localstorage added

--- src/createTableEntry.js
+++ src/createTableEntry.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var localstorage = require('./localStorage');
+
 function createTableEntry(msg) {
 
     var tr = document.createElement('tr');
@@ -9,19 +11,31 @@ function createTableEntry(msg) {
     var td2 = document.createElement('td');
     var td3 = document.createElement('td');
     var td4 = document.createElement('td');
+    var td5 = document.createElement('td');
 
     var span = document.createElement('span');
     span.innerHTML = '<a href="tel:' + msg.phone + '">' + msg.phone + '</a>';
-
     td1.appendChild(span);
+   
+
     td2.appendChild(document.createTextNode(msg.state.toUpperCase()));
     td3.appendChild(document.createTextNode(moment().format("h:mm a")));
     td4.appendChild(document.createTextNode('a few seconds ago'));
+    
+    var span2 = document.createElement('span');
+    span2.innerHTML = '<span style="cursor:pointer;" class="glyphicon glyphicon-star-empty text-center"></span>';
+
+    ////////
+    span2.onclick = function(){
+        console.log('click')
+    };
+    td5.appendChild(span2);
 
     tr.appendChild(td1);
     tr.appendChild(td2);
     tr.appendChild(td3);
     tr.appendChild(td4);
+    tr.appendChild(td5);
 
     setInterval(function() {
         td4.innerHTML = moment().from(created, true) + " ago"
---
+++ src/localStorage.js
@@ -0,0 +1,37 @@
+'use strict';
+
+function detectLocalStorage(){
+	return "localStorage" in window;
+}
+
+
+function savePhone(phone, time){
+	if(detectLocalStorage()){
+		var currentData = localStorage['data'];
+
+		if (currentData){
+			currentData = JSON.parse(localStorage['data']);
+			currentData.push({phone: phone, created: time});
+			localStorage['data'] = JSON.stringify(currentData);
+		}else{
+			var c = [];
+			c.push({phone: phone, created: time});
+			localStorage['data'] = JSON.stringify(c);
+		}
+	}
+}
+
+function getSavedPhones(){
+	if(detectLocalStorage()){
+		var currentData = localStorage['data'];
+
+		if(currentData){
+			return JSON.parse(currentData);
+		}
+	}
+}
+
+
+exports.savePhone = savePhone;
+exports.getSavedPhones = getSavedPhones;
+exports.detectLocalStorage = detectLocalStorage;
